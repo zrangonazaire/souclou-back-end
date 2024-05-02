df4417e7af0e219f8c989e20b05b35c0d6411c98
zrangonazaire <astairenazaire@gmail.com>
2024-05-02 16:19:03 +0000

    first commit

---
+++ src/main/java/com/souclou/SouclouApplication.java
@@ -0,0 +1,13 @@
+package com.souclou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SouclouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SouclouApplication.class, args);
+	}
+
+}
